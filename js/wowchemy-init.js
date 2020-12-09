89f0c3bd625d15e80412fcdfdcc41d83f311d20e
vork <vork@users.noreply.github.com>
2020-12-09 08:13:20 +0000

    Publishing Site markboss.me at c97bbecd167db1eb48a69360f1fe69bca6f72ee7 on Wed Dec  9 08:13:20 UTC 2020

--- js/wowchemy-init.js
+++ js/wowchemy-init.js
@@ -1,7 +1,7 @@
 (() => {
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201205224343-d70da8919962/assets/js/wowchemy-theming.js
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201208221717-1d5456668842/assets/js/wowchemy-theming.js
   function getThemeMode() {
-    return parseInt(localStorage.getItem("dark_mode") || 2);
+    return parseInt(localStorage.getItem("wcTheme") || 2);
   }
   function canChangeTheme() {
     return Boolean(window.wc.darkLightEnabled);
